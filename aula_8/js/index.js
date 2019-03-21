8cd4dd1815da809b35c9b35f892071afa6a5350d
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-03-21 20:10:07 -0300

    Add aula_9

--- aula_8/js/index.js
+++ aula_8/js/index.js
@@ -3,14 +3,33 @@ start();
 function start() {
     var btn = document.getElementById('btnName');
     btn.onclick = function() {
-        var inputName = document.getElementById('name');
+        var inputName = document.getElementById("name");
         //alert(inputName.value);
 
-        if (inputName.value == '') {
+        if (inputName.value === '') {
             inputName.value = 'input empty!';
         } else {
-            inputName.value = inputName.value.toUpperCase();
+            //inputName.value = inputName.value.toUpperCase();
         }
+
+        if (inputName.value.startsWith('R')) {
+            console.log('Seu nome tem uma letra bonita!');
+        }
+
+        var names = inputName.value.split(' ');
+        console.log(names[names.length - 1]);
+
+        var newValue = inputName.value.substring(1, inputName.value.length - 1);
+        console.log(newValue);
+
+        newValue = inputName.value.trim();
+        console.log(newValue);
+
+        var index = inputName.value.indexOf('@');
+        console.log(index);
+
+        index = inputName.value.search(/[áàíóôêé]/g);
+        console.log(index);
 
         console.log("I'm here!");
     };
